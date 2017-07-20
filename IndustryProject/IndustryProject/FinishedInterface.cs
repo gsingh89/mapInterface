8a5824e915272cda610022c0c7cdc00e9cf2425f
Tyler <Tyler@TYLER-PC>
2017-07-20 09:11:00 -0500

    Validating

--- IndustryProject/IndustryProject/FinishedInterface.cs
+++ IndustryProject/IndustryProject/FinishedInterface.cs
@@ -56,7 +56,7 @@ namespace IndustryProject
         private void btnSearch_Click(object sender, EventArgs e)
         {
             string enteredName = txtSearch.Text;
-            
+
             string basicQuery = @"SELECT NAMES.NAME_ACTUAL AS 'Geographical Name', 
                         NAME_PLACES.FEATURE_ID AS 'Unique National Identifier', 
                         PLACES.FEAT_CODE AS 'Feature Code', PLACES.MS250 AS 'NTS 250000 Map Sheet',
@@ -72,33 +72,6 @@ namespace IndustryProject
                         FROM NAMES JOIN NAME_PLACES ON NAMES.NAME_ID = NAME_PLACES.NAME_ID JOIN PLACES
                         ON PLACES.PLACE_ID = NAME_PLACES.PLACE_ID LEFT JOIN CASUALTIES ON NAMES.CASUALTY_ID = CASUALTIES.CASUALTY_ID
                         LEFT JOIN FEATURE_TYPES ON PLACES.FEAT_CODE = FEATURE_TYPES.FEAT_CODE ";
-
-        //new ObjectParameter("name", NAMES.NAME_ACTUAL),
-        //new ObjectParameter("ident", NAME_PLACES.FEATURE_ID)),
-        //new ObjectParameter("featcode", PLACES.FEAT_CODE),
-        //new ObjectParameter("ms250", PLACES.MS250),
-        //new ObjectParameter("ms50", PLACES.MS50),
-        //new ObjectParameter("latdeg", PLACES.LAT_DEG),
-        //new ObjectParameter("latmin", PLACES.LAT_MIN),
-        //new ObjectParameter("latsec", PLACES.LAT_SEC),
-        //new ObjectParameter("longdeg", PLACES.LONG_DEG),
-        //new ObjectParameter("longmin", PLACES.LONG_MIN),
-        //new ObjectParameter("longsec", PLACES.LONG_SEC),
-        //new ObjectParameter("casualhome", CASUALTIES.COMMUNITY),
-        //new ObjectParameter("casualreg", CASUALTIES.REG_NO),
-        //new ObjectParameter("casualrank", CASUALTIES.RANK_CASUALTY),
-        //new ObjectParameter("casualsurname", CASUALTIES.SURNAME),
-        //new ObjectParameter("casualgivname", CASUALTIES.GIVNAME),
-        //new ObjectParameter("casualdate", CASUALTIES.DATE_DECEASED),
-        //new ObjectParameter("casualregiment", CASUALTIES.SERVED),
-        //new ObjectParameter("casualburied", CASUALTIES.BURIED),
-        //new ObjectParameter("feattype", FEATURE_TYPES.FEAT_TYPE),
-        //new ObjectParameter("featdesc", FEATURE_TYPES.DESCR),
-        //new ObjectParameter("longit", PLACES.LONGITUDE),
-        //new ObjectParameter("latit", PLACES.LATITUDE));
-            
-
-            //basicQuery.Parameters.Add(new ObjectParameter("ln", "Adams"));
 
             if (radName.Checked && !String.IsNullOrWhiteSpace(enteredName))
             {
